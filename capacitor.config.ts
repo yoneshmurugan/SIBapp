6d05aec5c1e792785f0109b620dd9d7abb9dd461
yoneshmurugan <yoneshmurugan@gmail.com>
2026-06-12 11:50:37 +0100

    Updated Bundle ID to match Apple Developer Portal

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,8 +1,8 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'com.sib.app',
-  appName: 'SIB',
+  appId: 'com.senguntharinbusiness.app',
+  appName: 'SIBapp',
   webDir: 'dist',
   plugins: {
     CapacitorHttp: {
