e93390515063b511ea0f621303da8f5c3c3209f2
yoneshmurugan <yoneshmurugan@gmail.com>
2026-07-04 11:48:40 +0530

    fix: resolve jsx parsing error by adding missing closing div in ViewProfile

--- src/ProfilePage/ViewProfile.jsx
+++ src/ProfilePage/ViewProfile.jsx
@@ -303,6 +303,7 @@ export default function ViewProfile() {
                     )}
                   </div>
                 </div>
+                </div>
               </div>
 
               {/* Name & Title */}
